a5a628c50d1a6b4fc87ffa0a2c0f38a77eb1ca87
kjhgfdgtyhujhgfvd <40675551+soondjnf@users.noreply.github.com>
2018-08-22 18:09:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 client.on('message', async message => {
-var prefix = "#";
   if(message.content.startsWith(prefix + "تقديم")) {
     await message.channel.send("**:writing_hand: ارسل اللغة البرمجة الآن **").then(e => {
     let filter = m => m.author.id === message.author.id
@@ -24,7 +22,7 @@ let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors:
   md = co.first().content
         co.first().delete()
         e.edit(`**ارسل م تعرف بهذه اللغة الان**`)
-let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
 .then(col => {
   br = col.first().content
         col.first().delete()
@@ -32,30 +30,39 @@ e.edit("**جاري التقديم علي طلبك...**").then(b => {
         setTimeout(() => {
   b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
         },2000);
-  var star = client.channels.get('481528389916950539')
-
-if(!star) return;
-if(star) {
-star.send({embed : new Discord.RichEmbed()
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
 .setDescription(`**اللغة البرمجية : \n ${lan}\n مدة ممارسة هذه اللغة :\n ${md} \nالخبرة  :\n ${br}  **`)  
-          .setFooter(`star code.`)
+          .setFooter(`star.`)
 .setTimestamp()
 });
-}        
-})
-})
-})
-})
-})
- }
- 
-   client.on('message',async message => {
+
+client.on('message',async message => {
+let mention = message.mentions.members.first();
+let acRoom = client.channels.get('474697535013781565');
+if(message.content.startsWith(prefix + "رفض")) {
+if(message.guild.id !== '473110787230269481') return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+if(!mention) return message.reply("منشن شخص");
+
+var embed = new Discord.RichEmbed()
+.setTitle(':x:| تم رفض شخص')
+.addField('» تم رفض', `${mention}`,true)
+.addField('» بواسطة', `${message.author}`,true)
+.setFooter(`© SBot. 2018`);
+acRoom.send(embed);
+}
+});
+
+client.on('message',async message => {
   let mention = message.mentions.members.first();
   let role = message.content.split(" ").slice(2).join(" ");
   let mySupport = message.guild.roles.find('name',role);
-  let acRoom = client.channels.get('481528389916950539');
+  let acRoom = client.channels.get('474697535013781565');
   if(message.content.startsWith(prefix + "قبول")) {
-    if(message.guild.id !== '477397483904040995') return;
+    if(message.guild.id !== '473110787230269481') return;
     if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
     if(!mention) return message.reply('منشن شخص');
     if(!role) return message.reply('ادخل اسم رتبة');
@@ -63,20 +70,13 @@ star.send({embed : new Discord.RichEmbed()
     if(mention.roles.has(mySupport)) return message.reply('هذا الشخص معه الرتبة مسبقا');
 
     mention.addRole(mySupport).then(() => {
-      acRoom.send(`**[ ${mySupport} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+      var embed = new Discord.RichEmbed()
+      .setTitle(':white_check_mark:| تم قبول شخص')
+      .addField('» تم قبول', `${mention}`,true)
+      .addField('» بواسطة', `${message.author}`,true)
+      .setFooter(`© SBot. 2018`);
+      acRoom.send(embed);
     });
-  }
-});
-  
-    client.on('message',async message => {
-  let mention = message.mentions.members.first();
-  let acRoom = client.channels.get('481528389916950539');
-  if(message.content.startsWith(prefix + "رفض")) {
-  if(message.guild.id !== '477397483904040995') return;
-  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
-  if(!mention) return message.reply("منشن شخص");
-
-  acRoom.send(`**${mention} تم رفضك للاسف**`)
   }
 });
 
