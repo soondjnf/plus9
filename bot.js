bbe3076fcf1e0868c72187fe7f8282608cc4f01a
kjhgfdgtyhujhgfvd <40675551+soondjnf@users.noreply.github.com>
2018-09-06 14:29:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,5 +19,24 @@ console.log("Welcome Again !");
 }).catch(console.error)
 })
 
+
+
+
+
+ client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+هذا الموقع يعطيك حسابات فورتنايت ماين كرافت سبوتفي وكل شي
+هذا الرابط
+         https://to.free-gg.com/pTXzMgd                    
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+})
+
 client.login(process.env.BOT_TOKEN);
 
